76921ee687e365fe85d8269320b1917bfb399c6b
edward487 <drawdej963@gmail.com>
2017-02-08 20:46:25 -0800

    added create account

---
+++ js/createaccount.js
@@ -0,0 +1,66 @@
+$(window).ready(function () {
+var validator = $('#createaccount').bootstrapValidator({
+  	feedbackIcons : {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+      //validations
+    fields : {
+      email : {
+        validators : {
+        	notEmpty : {
+        		message : "Please provide an email address"
+        	},
+          regexp: {
+            regexp: '^[^@\\s]+@([^@\\s]+\\.)+[^@\\s]+$',
+          }
+     		}
+     	},
+     	pwd : {
+     		validators : {
+     			notEmpty : {
+     				message : "Please provide a password"
+     			},
+     			stringLength : {
+     				min : 8,
+     				message : "The password must be 8 characters long"
+     			},
+     			different : {
+     				field : "email",
+     				message : "Password too weak"
+     			}
+     		}
+     	},
+			cpwd : {
+     		validators : {
+     			notEmpty : {
+     				message : "Please retype provide a password"
+     			},
+     			stringLength : {
+     				min : 8,
+     				message : "The password must be 8 characters long"
+     			},
+     			identical : {
+     				field : "Password",
+     				message : "Password do not match"
+     			}
+     		}
+     	},
+     	fname : {
+     		validators : {
+     			notEmpty : {
+     				message : "Please provide a value"
+     			},
+     		}
+     	},
+     	lname : {
+     		validators : {
+     			notEmpty : {
+     				message : "Please provide a value"
+     			}
+     		}
+     	}
+    }
+  });
+});
