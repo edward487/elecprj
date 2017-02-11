e4428783fa8065e4d3ec19936caac6df4803a930
edward487 <drawdej963@gmail.com>
2017-02-10 16:37:37 -0800

    added page turner

---
+++ cur-first-year.php
@@ -0,0 +1,96 @@
+ <!DOCTYPE html>
+  <html lang="en" class="js csstransforms csstransforms3d">
+  <head>
+    <title>Create Account</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/cur-first-year.css">
+     
+    <script src="vendor/jquery/jquery-3.1.1.js"></script>
+    <script type="text/javascript" src="vendor/turnjs4/lib/turn.min.js"></script>
+    <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/navbar.js"></script>
+      
+</style>
+  </head>
+  <body>
+       <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#trends">Trends</a></li>
+            <li><a href="#faculty">Faculty</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Curriculum<span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="cur-first-year.php">First Year</a></li>
+                <li><a href="#">Second Year</a></li>
+                <li><a href="#">Third Year</a></li>
+                <li><a href="#">Fourth Year</a></li>
+                <li><a href="#">Fifth Year</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="createaccount.php">Sign up</a></li>
+            <li><a href="login.php">Log in</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+      </nav><hr><hr><br>
+    <div class="container">
+        <div id="magazine">
+            <div style="background-image:url('img/IMG_2344.JPG');"></div>
+            <div style="background-image:url('img/IMG_2345.JPG');"></div>
+            <div style="background-image:url('img/IMG_2346.JPG');"></div>
+            <div style="background-image:url('img/IMG_2347.JPG');"></div>
+            <div style="background-image:url('img/IMG_2348.JPG');"></div>
+            <div style="background-image:url('img/IMG_2348.JPG');"></div>
+        </div>
+    </div>
+<script>
+    	$(window).ready(function() {
+		$('#magazine').turn({
+							display: 'double',
+							acceleration: true,
+                            width: 400,
+                            height: 300,
+                            autoCenter: true,
+							gradients: !$.isTouch,
+							elevation:50,
+							when: {
+								turned: function(e, page) {
+									console.log('Current view: ', $(this).turn('view'));
+								}
+							}
+                                });
+
+
+	});
+	
+	
+	$(window).bind('keydown', function(e){
+		
+		if (e.keyCode==37)
+			$('#magazine').turn('previous');
+		else if (e.keyCode==39)
+			$('#magazine').turn('next');
+			
+	});
+</script>
+  </body>
+ </html>
+ 
