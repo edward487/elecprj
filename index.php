c566b1e985a99616dfffbca5fc33281658e6405e
Jerome Ng <jxandrexng@gmail.com>
2017-02-25 01:42:02 +0800

    added carousel to homepage

--- index.php
+++ index.php
@@ -13,7 +13,44 @@
           <div align="center">
               <h1><strong>COMPUTER ENGINEERING</strong></h1>
           </div>
-          
+          <div class="row">
+            <div class="col-lg-12">
+                <div id="slider" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner" role="listbox">
+                        <div class="item active">
+                            <img src="img/home1.jpg">
+                            <div class="carousel-caption">
+                                <h1>Hardware</h1>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="img/home2.jpg">
+                            <div class="carousel-caption">
+                                <h1>Hardware2</h1>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="img/home3.jpg">
+                            <div class="carousel-caption">
+                                <h1>Hardware3</h1>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="img/home4.jpg">
+                            <div class="carousel-caption">
+                                <h1>Hardware4</h1>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img src="img/home5.jpg">
+                            <div class="carousel-caption">
+                                <h1>Hardware5</h1>
+                            </div>
+                        </div>  
+                    </div>      
+                </div>
+            </div>
+          </div>
       </div> 
   </body>
     <script src="vendor/jquery/jquery.min.js"></script>
