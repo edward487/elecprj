53f35de59ed841d3d30fdb620f644a9ee095053c
edward487 <drawdej963@gmail.com>
2017-02-25 11:20:00 -0800

    fixed carousel

--- index.php
+++ index.php
@@ -13,45 +13,60 @@
           <div align="center">
               <h1><strong>COMPUTER ENGINEERING</strong></h1>
           </div>
-          <div class="row">
-            <div class="col-lg-12">
-                <div id="slider" class="carousel slide" data-ride="carousel">
+          <div class="row position1">
+             <div id="gallery-carousel" class="content-section text-center">
+                  <div class="col-lg-8 col-lg-offset-2">
+                    <h2>Gallery</h2>
+                  </div>
+                  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                      <li data-target="#myCarousel" data-slide-to="3"></li>
+                      <li data-target="#myCarousel" data-slide-to="4"></li>
+                      <li data-target="#myCarousel" data-slide-to="5"></li>
+                    </ol>
+
+                    <!-- Wrapper for slides -->
                     <div class="carousel-inner" role="listbox">
-                        <div class="item active">
-                            <img src="img/home1.jpg">
-                            <div class="carousel-caption">
-                                <h1>Hardware</h1>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="img/home2.jpg">
-                            <div class="carousel-caption">
-                                <h1>Hardware2</h1>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="img/home3.jpg">
-                            <div class="carousel-caption">
-                                <h1>Hardware3</h1>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="img/home4.jpg">
-                            <div class="carousel-caption">
-                                <h1>Hardware4</h1>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img src="img/home5.jpg">
-                            <div class="carousel-caption">
-                                <h1>Hardware5</h1>
-                            </div>
-                        </div>  
-                    </div>      
-                </div>
+                      <div class="item active">
+                        <img class="fillwidth " src="img/home1.jpg" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img  class="fillwidth " src="img/home2.jpg" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img class="fillwidth " src="img/home3.jpg" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img  class="fillwidth " src="img/home4.jpg" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img  class="fillwidth " src="img/home5.jpg" alt="JME">
+                      </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+              </div>
             </div>
           </div>
-      </div> 
+      </div><br><br><br><br><Br><br><br><br><br><br><Br>
+      <?php include'footer.php'?>
   </body>
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
