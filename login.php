30db8c42da09f5a6a66f458667044149ec46364f
edward487 <drawdej963@gmail.com>
2017-02-24 21:58:16 -0800

    change in log in

--- login.php
+++ login.php
@@ -12,52 +12,42 @@
       <?php include 'navbar.php' ?><br><br>
     <div class="container" style="margin-top:40px">
             <div class="row">
-                <div class="col-sm-6 col-md-4 col-md-offset-4">
-                    <div class="animate animated zoomIn blur-bg">
-                        <div class="panel-heading" align="center">
-                            <h4><strong>Sign in to our Website</strong></h4>
-                        </div>
-                        <div class="panel-body ">
-                            <form role="form" action="#" method="POST">
-                                <fieldset class="blur-bg">
-                                    <div class="row">
-                                        <div class="center-block">
-                                            <!--Logo natin ang ilalagay dito-->
-                                            <img class="profile-img"
-                                                src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-12 col-md-10  col-md-offset-1 ">
-                                            <div class="form-group">
-                                                <div class="input-group">
-                                                    <span class="input-group-addon">
-                                                        <i class="glyphicon glyphicon-user"></i>
-                                                    </span>
-                                                    <input class="form-control" placeholder="Username" name="loginname" type="text" autofocus>
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <div class="input-group">
-                                                    <span class="input-group-addon">
-                                                        <i class="glyphicon glyphicon-lock"></i>
-                                                    </span>
-                                                    <input class="form-control" placeholder="Password" name="password" type="password" value="">
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <input type="submit" class="btn btn-success btn-block" value="Sign in">
+                    <div class="col-sm-6 col-md-4 col-md-offset-4 blur">
+                        <div class="animate animated fadeIn">
+                            <div class="panel-heading" align="center">
+                                <h4><strong>Sign in to our Website</strong></h4>
+                            </div>
+                            <div class="panel-body ">
+                                <form role="form" action="#" method="POST">
+                                    <fieldset>
+                                        <div class="row">
+                                            <div class="center-block">
+                                                <!--Logo natin ang ilalagay dito-->
+                                                <img class="profile-img"
+                                                    src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt="">
                                             </div>
                                         </div>
-                                    </div>
-                                </fieldset>
-                            </form>
-                        </div>
-                        <div align="center" class="bottom-footer">
-                            Don't have an account? <a href="createaccount.php" onClick=""> Sign Up Here </a>
+                                        <div class="row">
+                                            <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                                                <div class="form-group">
+                                                        <input class="blur-textbox" placeholder="Username" name="loginname" type="text" autofocus>
+                                                </div>
+                                                <div class="form-group">
+                                                        <input class="blur-textbox" placeholder="Password" name="password" type="password" value="">
+                                                </div>
+                                                <div class="form-group">
+                                                    <input type="submit" class="blur-login btn-block" value="Log in">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </fieldset>
+                                </form>
+                            </div>
+                            <div align="center" class="bottom-footer">
+                                Don't have an account? <a href="createaccount.php" onClick=""> Sign Up Here </a>
+                            </div>
                         </div>
                     </div>
-                </div>
             </div>
         </div>
      </body>
