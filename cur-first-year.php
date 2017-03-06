76a783bc8cf13a6a7f3fb40c672fdbe7e8f3133e
Jerome Ng <jxandrexng@gmail.com>
2017-03-06 22:43:21 +0800

    pull

--- cur-first-year.php
+++ cur-first-year.php
@@ -31,47 +31,55 @@
       
   </style>
   <body>
-      <br><br><br><br>
-    <div class="container">
-        <div id="magazine">
-            <div style="background-image:url('img/IMG_2344.JPG');"></div>
-            <div style="background-image:url('img/IMG_2345.JPG');"></div>
-            <div style="background-image:url('img/IMG_2346.JPG');"></div>
-            <div style="background-image:url('img/IMG_2347.JPG');"></div>
-            <div style="background-image:url('img/IMG_2348.JPG');"></div>
-            <div style="background-image:url('img/IMG_2348.JPG');"></div>
-        </div>
-    </div>
-<script>
-    	$(window).ready(function() {
-		$('#magazine').turn({
-							display: 'double',
-							acceleration: true,
-                            width: 400,
-                            height: 300,
-                            autoCenter: true,
-							gradients: !$.isTouch,
-							elevation:50,
-							when: {
-								turned: function(e, page) {
-									console.log('Current view: ', $(this).turn('view'));
-								}
-							}
-                                });
+        <div class="row position1">
+             <div id="gallery-carousel" class="content-section text-center">
+                  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                      <li data-target="#myCarousel" data-slide-to="3"></li>
+                      <li data-target="#myCarousel" data-slide-to="4"></li>
+                      <li data-target="#myCarousel" data-slide-to="5"></li>
+                    </ol>
 
+                    <!-- Wrapper for slides -->
+                    <div class="carousel-inner" role="listbox">
+                      <div class="item active">
+                        <img class="fillwidth " src="img/IMG_2344.JPG" alt="JME">
+                      </div>
 
-	});
-	
-	
-	$(window).bind('keydown', function(e){
-		
-		if (e.keyCode==37)
-			$('#magazine').turn('previous');
-		else if (e.keyCode==39)
-			$('#magazine').turn('next');
-			
-	});
-</script><br><br><br><br>
+                      <div class="item">
+                        <img  class="fillwidth " src="img/IMG_2345.JPG" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img class="fillwidth " src="img/IMG_2346.JPG" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img  class="fillwidth " src="img/IMG_2347.JPG" alt="JME">
+                      </div>
+
+                      <div class="item">
+                        <img  class="fillwidth " src="img/IMG_2348.JPG" alt="JME">
+                      </div>
+                    </div>
+
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+              </div>
+            </div>
+          </div>
       <div class="container">
         <div class="col-md-12">
           <h1><strong>FIRST YEAR CURRICULUM</strong></h1>
