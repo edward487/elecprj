f3b8461821165f36669a803c5e516a901197fde8
Jerome Ng <jxandrexng@gmail.com>
2017-03-11 21:38:21 +0800

    Added image boxes in job page

--- job.php
+++ job.php
@@ -5,43 +5,208 @@
     <title>Job Opportunities</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/job.css">
   </head>
-    <style>
-        body {
-            background: url('img/back7.png');
-            background-attachment: fixed;
-            overflow: hidden;
-        }
-
-        canvas {
-            position: absolute;
-            backface-visibility: hidden;
-            -webkit-backface-visibility: hidden;
-            -moz-backface-visibility: hidden;
-            -ms-backface-visibility: hidden;
-        }
-
-        img {
-            position: relative;
-            cursor: pointer;
-        }
-
-        #container {
-            position: absolute;
-            width: 768px;
-            height: 485px;
-            left: 0;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            margin: auto;
-        }
-    </style>
-<body><br><br><br>
-    <div id="container">
-    </div><br><br><br>
-    <script src="js/job.js"></script>
-    <script src="js/job2.js"></script>
-    <script src="js/job3.js"></script>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- Left job opportunities grid -->
+                <div class="row">
+                <div class="col-md-6">
+                    <div align="center">
+                        <h1><strong>HARDWARE ENGINEER</strong></h1>
+                        <img src="img/job6.jpg" height="300px">
+                        <br><br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#hardwareEngineer">SEE MORE</button>
+                        <div class="modal fade" id="hardwareEngineer" tabindex="-1" role="dialog" aria-labelledby="hardwareEngineer" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content modal-md">
+                                    <div class="modal-header blur" align="center">
+                                        <h5 class="modal-title" id="hardwareEngineerLabel"> 
+                                            HARDWARE ENGINEER
+                                        </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body blur-body">
+                                    <p align="justify">
+                                        &emsp;&emsp;&emsp;
+                                        Computer hardware engineers research, design, develop, and test computer systems and components such as processors, circuit boards, memory devices, networks, and routers. These engineers discover new directions in computer hardware, which generate rapid advances in computer technology.
+                                    </p>
+                                </div>
+                                <div class="modal-footer blur">
+                                    <button type="button" class="btn blur-button" data-dismiss="modal">OK</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br><br><br><br>
+                    <div class="col-md-12" align="center">
+                        <h3><strong>Jobs Under Hardware Engineers</strong></h3>
+                    </div>
+                    <div class="col-md-6" align="center">
+                        <h4><strong>CIRCUIT DESIGN</strong></h4>
+                        <img src="img/job9.jpg" height="150px" width="250px">
+                        <br><br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#circuitDesign">SEE MORE</button>
+                        <div class="modal fade" id="circuitDesign" tabindex="-1" role="dialog" aria-labelledby="circuitDesign" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header blur">
+                                        <h5 class="modal-title" id="programmer"> 
+                                            CIRCUIT DESIGN
+                                        </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body blur-body">
+                                    <p align="justify">
+                                        &emsp;&emsp;&emsp;
+                                        Circuit design can cover systems ranging from complex electronic systems all the way down to the individual transistors within an integrated circuit. For simple circuits the design process can often be done by one person without needing a planned or structured design process, but for more complex designs, teams of designers following a systematic approach with intelligently guided computer simulation are becoming increasingly common. In integrated circuit design automation, the term "circuit design" often refers to the step of the design cycle which outputs the schematics of the integrated circuit.
+                                    </p>
+                                </div>
+                                <div class="modal-footer blur">
+                                    <button type="button" class="btn blur-button" data-dismiss="modal">OK</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6" align="center">
+                        <h4><strong>COMPUTER DESIGN</strong></h4>
+                        <img src="img/job10.jpg" height="150px" width="250px">
+                        <br><br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#computerDesign">SEE MORE</button>
+                        <div class="modal fade" id="computerDesign" tabindex="-1" role="dialog" aria-labelledby="computerDesign" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header blur">
+                                        <h5 class="modal-title" id="programmer"> 
+                                            COMPUTER DESIGN
+                                        </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body blur-body">
+                                    <p align="justify">
+                                        &emsp;&emsp;&emsp;
+                                        Computer architecture is a set of rules and methods that describe the functionality, organization, and implementation of computer systems. Some definitions of architecture define it as describing the capabilities and programming model of a computer but not a particular implementation.
+                                    </p>
+                                </div>
+                                <div class="modal-footer blur">
+                                    <button type="button" class="btn blur-button" data-dismiss="modal">OK</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of left job opportunities grid -->
+                <!-- Right job opportunities grid -->
+                <div class="col-md-6">
+                    <div align="center">
+                        <h1><strong>SOFTWARE ENGINEER</strong></h1>
+                        <img src="img/job5.jpg" height="300px">
+                        <br><br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#softwareEngineer">SEE MORE</button>
+                        <div class="modal fade" id="softwareEngineer" tabindex="-1" role="dialog" aria-labelledby="softwareEngineer" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header blur">
+                                        <h5 class="modal-title" id="softwareEngineerLabel"> 
+                                            SOFTWARE ENGINEER
+                                        </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body blur-body">
+                                    <p align="justify">
+                                        &emsp;&emsp;&emsp;
+                                        This job is office-based, and most of the work is done during normal office hours, but can sometimes lead to working away and working late or during weekends, depending on where and when the client is situated. The job can also be done at home or anywhere a computer is set up. Some high-profile companies have encouraged software engineers to work for long hours; Apple's Steve Jobs set up a culture where engineers would never take holidays and work throughout weekends, yet love what they were doing.
+                                    </p>
+                                </div>
+                                <div class="modal-footer blur">
+                                    <button type="button" class="btn blur-button" data-dismiss="modal">OK</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br><br><br><br>
+                    <div class="col-md-12" align="center">
+                        <h3><strong>Jobs Under Software Engineers</strong></h3>
+                    </div>
+                    <div class="col-md-6" align="center">
+                        <h4><strong>PROGRAMMING</strong></h4>
+                        <img src="img/job7.jpg" height="150px" width="250px">
+                        <br><br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#programmer">SEE MORE</button>
+                        <div class="modal fade" id="programmer" tabindex="-1" role="dialog" aria-labelledby="programmer" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header blur">
+                                        <h5 class="modal-title" id="programmer"> 
+                                            PROGRAMMING
+                                        </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body blur-body">
+                                    <p align="justify">
+                                        &emsp;&emsp;&emsp;
+                                        The term computer programmer can refer to a specialist in one area of computer programming or to a generalist who writes code for many kinds of software. One who practices or professes a formal approach to programming may also be known as a programmer analyst. A programmer's primary computer language (Assembly, COBOL, C, C++, C#, Java, Lisp, Python, etc.) is often prefixed to these titles, and those who work in a Web environment often prefix their titles with Web. The term programmer can be used to refer to a software developer, Web developer, mobile applications developer, embedded firmware developer, software engineer, computer scientist, or software analyst.
+                                    </p>
+                                </div>
+                                <div class="modal-footer blur">
+                                    <button type="button" class="btn blur-button" data-dismiss="modal">OK</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6" align="center">
+                        <h4><strong>WEB DEVELOPMENT</strong></h4>
+                        <img src="img/job8.jpg" height="150px" width="250px">
+                        <br><br><br>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#webDeveloper">SEE MORE</button>
+                        <div class="modal fade" id="webDeveloper" tabindex="-1" role="dialog" aria-labelledby="webDeveloper" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header blur">
+                                        <h5 class="modal-title" id="webDeveloper"> 
+                                            WEB DEVELOPMENT
+                                        </h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body blur-body">
+                                    <p align="justify">
+                                        &emsp;&emsp;&emsp;
+                                        A web developer is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications, or distributed network applications that are run over HTTP from a web server to a web browser.
+                                    </p>
+                                </div>
+                                <div class="modal-footer blur">
+                                    <button type="button" class="btn blur-button" data-dismiss="modal">OK</button>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <!-- End of right job opportunities grid -->
+            </div>
+        </div>
+        <!-- End of row -->
+    </div>
 </body>
 </html>
